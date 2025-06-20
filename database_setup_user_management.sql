ad1693c7b408c277dd753b689717c97489dbb422
focuszero1 <ushergay@gmail.com>
2025-06-20 15:07:21 -0400

    feat: Add trigger and function for automatic profile creation on new user sign-up

---
+++ database_setup_user_management.sql
@@ -0,0 +1,22 @@
+-- Creates a trigger function for new user sign-ups
+-- This function is called by a trigger on the auth.users table
+-- It creates a corresponding entry in the public.profiles table
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+SECURITY DEFINER
+SET search_path = public
+AS $$
+BEGIN
+  INSERT INTO public.profiles (id, display_name)
+  VALUES (new.id, new.raw_user_meta_data->>'display_name');
+  RETURN new;
+END;
+$$;
+
+-- Creates the trigger that calls the function
+-- This trigger is fired after a new user is inserted into auth.users
+CREATE OR REPLACE TRIGGER on_auth_user_created
+  AFTER INSERT ON auth.users
+  FOR EACH ROW EXECUTE FUNCTION public.handle_new_user(); 
